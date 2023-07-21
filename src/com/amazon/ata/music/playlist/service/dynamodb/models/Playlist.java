82e4ff0b65bf0f55240b99136015ed72d2619c39
chmsly <chris.c.hemsley@gmail.com>
2023-07-21 14:45:47 -0600

    MT

--- src/com/amazon/ata/music/playlist/service/dynamodb/models/Playlist.java
+++ src/com/amazon/ata/music/playlist/service/dynamodb/models/Playlist.java
@@ -2,11 +2,9 @@ package com.amazon.ata.music.playlist.service.dynamodb.models;
 
 import com.amazon.ata.music.playlist.service.converters.AlbumTrackLinkedListConverter;
 
-import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBAttribute;
-import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBHashKey;
-import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBTable;
-import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBTypeConverted;
+import com.amazonaws.services.dynamodbv2.datamodeling.*;
 
+import java.util.Set;
 import java.util.List;
 
 /**
@@ -16,11 +14,42 @@ import java.util.List;
 public class Playlist {
     private String id;
     private List<AlbumTrack> songList;
+    private String name;
+    private String customerId;
+    private Integer songCount;
+    private Set<String> tags;
+
+
+
 
     @DynamoDBHashKey(attributeName = "id")
     public String getId() {
         return id;
     }
+
+    @DynamoDBRangeKey
+    public String getCustomerId() {
+        return customerId;
+    }
+
+    @DynamoDBAttribute(attributeName = "name")
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @DynamoDBAttribute(attributeName = "tags")
+    public Set<String> getTags() {
+        return tags;
+    }
+
+    public void setTags(Set<String> tags) {
+        this.tags = tags;
+    }
+
 
     public void setId(String id) {
         this.id = id;
