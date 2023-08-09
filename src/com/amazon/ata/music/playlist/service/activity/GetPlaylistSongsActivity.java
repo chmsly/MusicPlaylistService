f66242ec4cb92a34efefad254cba470f37869d8d
chmsly <chris.c.hemsley@gmail.com>
2023-08-09 12:25:11 -0600

    MT4

--- src/com/amazon/ata/music/playlist/service/activity/GetPlaylistSongsActivity.java
+++ src/com/amazon/ata/music/playlist/service/activity/GetPlaylistSongsActivity.java
@@ -1,5 +1,10 @@
 package com.amazon.ata.music.playlist.service.activity;
 
+import com.amazon.ata.music.playlist.service.converters.ModelConverter;
+import com.amazon.ata.music.playlist.service.dynamodb.models.AlbumTrack;
+import com.amazon.ata.music.playlist.service.dynamodb.models.Playlist;
+import com.amazon.ata.music.playlist.service.exceptions.PlaylistNotFoundException;
+import com.amazon.ata.music.playlist.service.models.SongOrder;
 import com.amazon.ata.music.playlist.service.models.requests.GetPlaylistSongsRequest;
 import com.amazon.ata.music.playlist.service.models.results.GetPlaylistSongsResult;
 import com.amazon.ata.music.playlist.service.models.SongModel;
@@ -12,6 +17,7 @@ import org.apache.logging.log4j.Logger;
 
 import javax.inject.Inject;
 import java.util.Collections;
+import java.util.List;
 
 /**
  * Implementation of the GetPlaylistSongsActivity for the MusicPlaylistService's GetPlaylistSongs API.
@@ -43,11 +49,40 @@ public class GetPlaylistSongsActivity implements RequestHandler<GetPlaylistSongs
      * @return getPlaylistSongsResult result object containing the playlist's list of API defined {@link SongModel}s
      */
     @Override
+//    public GetPlaylistSongsResult handleRequest(final GetPlaylistSongsRequest getPlaylistSongsRequest, Context context) {
+//        log.info("Received GetPlaylistSongsRequest {}", getPlaylistSongsRequest);
+//
+//        Playlist playlist = playlistDao.getPlaylist(getPlaylistSongsRequest.getId());
+//
+//
+//        return GetPlaylistSongsResult.builder()
+//                .withSongList(ModelConverter.toSongModel(playlist.getSongList()))
+//                .build();
+//    }
+//}
+
     public GetPlaylistSongsResult handleRequest(final GetPlaylistSongsRequest getPlaylistSongsRequest, Context context) {
         log.info("Received GetPlaylistSongsRequest {}", getPlaylistSongsRequest);
 
+        Playlist playlist = playlistDao.getPlaylist(getPlaylistSongsRequest.getId());
+        List<AlbumTrack> albumTrackList = playlist.getSongList();
+        SongOrder requestOrder = getPlaylistSongsRequest.getOrder();
+
+        if (requestOrder == null){
+            requestOrder = SongOrder.DEFAULT;
+        } else if (requestOrder == SongOrder.REVERSED) {
+            Collections.reverse(albumTrackList);
+
+        } else if (requestOrder == SongOrder.SHUFFLED) {
+            Collections.shuffle(albumTrackList);
+        }
+
+        if (playlist.getSongList() == null){
+            throw new PlaylistNotFoundException();
+        }
+
         return GetPlaylistSongsResult.builder()
-                .withSongList(Collections.singletonList(new SongModel()))
+                .withSongList(ModelConverter.toSongModel(albumTrackList))
                 .build();
     }
 }
